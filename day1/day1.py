c69c40427e52bd0dd936368c955319aba280d163
Jordan Christensen <thebigjc@gmail.com>
2022-12-01 11:54:58 -0500

    golf

--- day1/day1.py
+++ day1/day1.py
@@ -1,12 +1,7 @@
 from aocd import data, submit
-from typing import Iterator
 from more_itertools import take
 
-def make_elves(elves: Iterator[str]) -> Iterator[int]:
-    for l in elves:
-        yield sum(map(int, l.split("\n")))
-    
-top_3 = take(3, sorted(make_elves(data.split("\n\n")), reverse=True))
+top_3 = take(3, sorted(map(lambda x: sum(map(int, x.split("\n"))), data.split("\n\n")), reverse=True))
 
 submit(top_3[0], part="a", day=1, year=2022)
 submit(sum(top_3), part="b", day=1, year=2022)
