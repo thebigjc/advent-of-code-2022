e27e4541edc7edb81c410a763d9f2508fe32c73c
Jordan Christensen <thebigjc@gmail.com>
2022-12-01 11:54:01 -0500

    might as well split

--- day1/day1.py
+++ day1/day1.py
@@ -1,18 +1,12 @@
-from aocd import lines, submit
+from aocd import data, submit
 from typing import Iterator
-import heapq 
+from more_itertools import take
 
-def elves(lines: Iterator[str]) -> Iterator[int]:
-    s : int = 0
-    for l in lines:
-        if len(l) == 0:
-            yield s
-            s = 0
-        else:
-            s += int(l)
+def make_elves(elves: Iterator[str]) -> Iterator[int]:
+    for l in elves:
+        yield sum(map(int, l.split("\n")))
+    
+top_3 = take(3, sorted(make_elves(data.split("\n\n")), reverse=True))
 
-m : int = max(elves(lines))
-three : int = sum(heapq.nlargest(3, elves(lines)))
-
-submit(m, part="a", day=1, year=2022)
-submit(three, part="b", day=1, year=2022)
+submit(top_3[0], part="a", day=1, year=2022)
+submit(sum(top_3), part="b", day=1, year=2022)
