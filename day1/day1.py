f5724f2417414e049aba404dc27faab02182e177
Jordan Christensen <thebigjc@gmail.com>
2022-12-01 08:30:45 -0500

    day1

---
+++ day1/day1.py
@@ -0,0 +1,17 @@
+from aocd import lines, submit
+import heapq 
+
+def elves(lines):
+    s = 0
+    for l in lines:
+        if len(l) == 0:
+            yield s
+            s = 0
+        else:
+            s += int(l)
+
+m = max(elves(lines))
+three = sum(heapq.nlargest(3, elves(lines)))
+
+submit(m, part="a", day=1, year=2022)
+submit(three, part="b", day=1, year=2022)
