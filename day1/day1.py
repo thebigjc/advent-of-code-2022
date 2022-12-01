9a9aefef978967a9d7a357723c883f7d6167eb60
Jordan Christensen <thebigjc@gmail.com>
2022-12-01 11:59:37 -0500

    Revert "More types and .gitignore"
    This reverts commit f24f520d33c8d2b8715005659d0398bcf29baf46.

--- day1/day1.py
+++ day1/day1.py
@@ -1,7 +1,17 @@
 from aocd import data, submit
 from more_itertools import take
 
-top_3 = take(3, sorted(map(lambda x: sum(map(int, x.split("\n"))), data.split("\n\n")), reverse=True))
+def elves(lines: Iterator[str]) -> Iterator[int]:
+    s = 0
+    for l in lines:
+        if len(l) == 0:
+            yield s
+            s = 0
+        else:
+            s += int(l)
 
-submit(top_3[0], part="a", day=1, year=2022)
-submit(sum(top_3), part="b", day=1, year=2022)
+m = max(elves(lines))
+three = sum(heapq.nlargest(3, elves(lines)))
+
+submit(m, part="a", day=1, year=2022)
+submit(three, part="b", day=1, year=2022)
