080daf6426734150925a83d1053d968f465e005c
Jordan Christensen <thebigjc@gmail.com>
2022-12-04 10:44:59 -0500

    pylint

--- day4/day4.py
+++ day4/day4.py
@@ -2,12 +2,12 @@ from aocd import lines, submit
 
 
 def test_overlap():
-    assert overlap("2-4", "6-8") == 0
-    assert overlap("2-8", "3-7") == 5
-    assert overlap("6-6", "4-6") == 1
-    assert overlap("4-6", "6-6") == 1
-    assert overlap("5-7", "7-9") == 1
-    assert overlap("2-6", "4-8") == 3
+    assert overlap(range_set("2-4"), range_set("6-8")) == 0
+    assert overlap(range_set("2-8"), range_set("3-7")) == 5
+    assert overlap(range_set("6-6"), range_set("4-6")) == 1
+    assert overlap(range_set("4-6"), range_set("6-6")) == 1
+    assert overlap(range_set("5-7"), range_set("7-9")) == 1
+    assert overlap(range_set("2-6"), range_set("4-8")) == 3
 
 
 def test_parse_range():
@@ -24,32 +24,32 @@ def test_range_set():
     assert range_set("6-6") == {6}
 
 
-def parse_range(r: str) -> tuple[int, int]:
-    (a, b) = r.split("-")
-    return (int(a), int(b))
+def parse_range(ranges: str) -> tuple[int, int]:
+    (range_a, range_b) = ranges.split("-")
+    return (int(range_a), int(range_b))
 
 
-def range_set(r: str) -> set[int]:
-    (a, b) = parse_range(r)
-    return set(range(a, b + 1))
+def range_set(range_str: str) -> set[int]:
+    (range_a, range_b) = parse_range(range_str)
+    return set(range(range_a, range_b + 1))
 
 
-def overlap(a: str, b: str) -> int:
-    set_a = range_set(a)
-    set_b = range_set(b)
-
-    return len(set_a.intersection(set_b))
+def overlap(range_set_a: set[int], range_set_b: set[int]) -> int:
+    return len(range_set_a.intersection(range_set_b))
 
 
 if __name__ == "__main__":
-    n_a : int = 0
-    n_b : int = 0
+    n_a: int = 0
+    n_b: int = 0
 
     for l in lines:
         (a, b) = l.split(",")
 
-        l = overlap(a, b)
-        if l == len(a) or l == len(b):
+        set_a = range_set(a)
+        set_b = range_set(b)
+
+        l = overlap(set_a, set_b)
+        if l == len(set_a) or l == len(set_b):
             n_a += 1
         if l > 0:
             n_b += 1
