6e43ba1dcb90ed2bd447f9f8d7bb82e0549b5142
Jordan Christensen <thebigjc@gmail.com>
2022-12-05 13:43:23 -0500

    day5 kinda gross

---
+++ day5/day5.py
@@ -0,0 +1,63 @@
+from aocd import lines, submit
+import re
+from itertools import batched
+from parse import parse
+
+REGEX_MOVE = re.compile(r"move (\d+) from (\d+) to (\d+)")
+
+
+stacks_a = []
+stacks_b = []
+
+
+def push_bottom(stacks, stack, value):
+    while len(stacks) <= stack:
+        stacks.append([])
+    stacks[stack].insert(0, value)
+
+
+done_stacks = False
+
+for line in lines:
+    pushed_any = False
+    if not done_stacks:
+        stack_slots = batched(line, 4)
+        for i, stack_slot in enumerate(stack_slots):
+            if stack_slot[1].isupper():
+                push_bottom(stacks_a, i, stack_slot[1])
+                push_bottom(stacks_b, i, stack_slot[1])
+
+                pushed_any = True
+        if not pushed_any:
+            done_stacks = True
+    else:
+        p = parse("move {:d} from {:d} to {:d}", line)
+        if not p:
+            continue
+        (count, from_stack, to_stack) = p
+        for i in range(count):
+            stacks_a[to_stack - 1].append(stacks_a[from_stack - 1].pop())
+
+        print(f"Before B: {stacks_b[from_stack-1]}, {stacks_b[to_stack-1]}")
+        new = []
+        for i in range(count):
+            new.append(stacks_b[from_stack - 1].pop())
+        for i in range(count):
+            stacks_b[to_stack - 1].append(new.pop())
+        print(f"After B: {stacks_b[from_stack-1]}, {stacks_b[to_stack-1]}")
+
+
+def top_of_stacks(stacks_to_top):
+    top = []
+    for stack in stacks_to_top:
+        top.append(stack.pop())
+
+    answer = "".join(top)
+    return answer
+
+
+answer_a = top_of_stacks(stacks_a)
+submit(answer_a, part="a", day=5, year=2022)
+
+answer_b = top_of_stacks(stacks_b)
+submit(answer_b, part="b", day=5, year=2022)
