135ad70ef9fd720c56c8a3718c67036d5e7e1727
Jordan Christensen <thebigjc@gmail.com>
2022-12-07 10:30:19 -0500

    black and lint

--- day7/day7.py
+++ day7/day7.py
@@ -25,19 +25,21 @@ $ ls
 5626152 d.ext
 7214296 k"""
 
-def test_total_dirs():
-    all_dirs = parse_dirs(TEST.splitlines())
 
-    assert part_a(all_dirs) == 95437
+def test_parta_dirs():
+    test_dirs = parse_dirs(TEST.splitlines())
 
-def test_total_dirs():
-    all_dirs = parse_dirs(TEST.splitlines())
+    assert part_a(test_dirs) == 95437
 
-    assert part_b(all_dirs) == 24933642
+
+def test_partb_dirs():
+    test_dirs = parse_dirs(TEST.splitlines())
+
+    assert part_b(test_dirs) == 24933642
 
 
 class Dir:
-    def __init__(self, name : str, parent = None):
+    def __init__(self, name: str, parent=None):
         self.name = name
         self.parent = parent
         self.children = []
@@ -45,24 +47,24 @@ class Dir:
 
     def add_dir(self, child):
         self.children.append(child)
-    
-    def add_file(self, size : int):
+
+    def add_file(self, size: int):
         self.size += size
 
     def total_size(self, max_size: int = 100000):
-        return self.size + sum([child.total_size(max_size) for child in self.children])
+        return self.size + sum(child.total_size(max_size) for child in self.children)
 
 
-def parse_dirs(lines : list[str]) -> list[Dir]:
+def parse_dirs(dir_lines: list[str]) -> list[Dir]:
     root = Dir("/")
     cwd = root
 
-    all_dirs = [root]
+    all_dirs = [input]
 
-    for l in lines:
-        if l.startswith("$ cd "):
-            dir_name = l[5:]
-            new_dir = Dir(l[5:], cwd)
+    for line in dir_lines:
+        if line.startswith("$ cd "):
+            dir_name = line[5:]
+            new_dir = Dir(line[5:], cwd)
             if dir_name == "/":
                 cwd = root
             elif dir_name == "..":
@@ -71,13 +73,15 @@ def parse_dirs(lines : list[str]) -> list[Dir]:
                 all_dirs.append(new_dir)
                 cwd.add_dir(new_dir)
                 cwd = new_dir
-        elif p := parse("{:d} {}", l):
-            cwd.add_file(p[0])
+        elif parsed := parse("{:d} {}", line):
+            cwd.add_file(parsed[0])
 
     return all_dirs
 
-def part_a(dirs : list[Dir]) -> int:
-    return sum(d.total_size() for d in dirs if d.total_size() < 100000)
+
+def part_a(dirs_a: list[Dir]) -> int:
+    return sum(d.total_size() for d in dirs_a if d.total_size() < 100000)
+
 
 def part_b(all_dirs):
     free_space = 70000000 - max(d.total_size() for d in all_dirs)
@@ -85,9 +89,10 @@ def part_b(all_dirs):
 
     return min(d.total_size() for d in all_dirs if d.total_size() >= space_needed)
 
-all_dirs = parse_dirs(lines)
-s_a = part_a(all_dirs)
-s_b = part_b(all_dirs)
+if __name__ == "__main__":
+    dirs = parse_dirs(lines)
+    s_a = part_a(dirs)
+    s_b = part_b(dirs)
 
-submit(s_a, part="a", day=7, year=2022)
-submit(s_b, part="b", day=7, year=2022)
+    submit(s_a, part="a", day=7, year=2022)
+    submit(s_b, part="b", day=7, year=2022)
