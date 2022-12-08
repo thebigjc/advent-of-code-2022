633bb8a38b2892e7be5dd4ee890f39a42e3ede7b
Jordan Christensen <thebigjc@gmail.com>
2022-12-08 13:59:43 -0500

    add tests

--- day8/day8.py
+++ day8/day8.py
@@ -9,27 +9,17 @@ TEST = """30373
 33549
 35390""".splitlines()
 
-tree_lines = lines
-
-trees = []
-
-y_len = len(tree_lines)
-x_len = len(tree_lines[0])
-
 
 def to_coords(idx, y_max):
     return (idx % y_max, idx // y_max)
 
 
-def is_edge(idx):
+def is_edge(idx, x_len, y_len):
     (x_idx, y_idx) = to_coords(idx, y_len)
     return x_idx == 0 or x_idx == x_len - 1 or y_idx == 0 or y_idx == y_len - 1
 
 
-def tree_at(x_idx, y_idx):
-    assert 0 <= x_idx < x_len
-    assert 0 <= y_idx < y_len
-
+def tree_at(x_idx, y_idx, y_len, trees):
     return trees[x_idx + y_idx * y_len]
 
 
@@ -41,14 +31,14 @@ def visible_dir(height, start, end, step, tree_func):
     return visible
 
 
-def is_visible(height, idx):
+def is_visible(trees, height, idx, x_len, y_len):
     (x_idx, y_idx) = to_coords(idx, y_len)
 
     def horiz(x_idx):
-        return tree_at(x_idx, y_idx)
+        return tree_at(x_idx, y_idx, y_len, trees)
 
     def vert(y_idx):
-        return tree_at(x_idx, y_idx)
+        return tree_at(x_idx, y_idx, y_len, trees)
 
     visible_left = visible_dir(height, 0, x_idx, 1, horiz)
     visible_right = visible_dir(height, x_len - 1, x_idx, -1, horiz)
@@ -67,14 +57,14 @@ def score_tree(height, start, end, step, tree_func):
     return score
 
 
-def tree_score(height, idx):
+def tree_score(trees, height, idx, x_len, y_len):
     (x_idx, y_idx) = to_coords(idx, y_len)
 
     def horiz(x_idx):
-        return tree_at(x_idx, y_idx)
+        return tree_at(x_idx, y_idx, y_len, trees)
 
     def vert(y_idx):
-        return tree_at(x_idx, y_idx)
+        return tree_at(x_idx, y_idx, y_len, trees)
 
     score_left = score_tree(height, x_idx - 1, -1, -1, horiz)
     score_right = score_tree(height, x_idx + 1, x_len, 1, horiz)
@@ -83,13 +73,33 @@ def tree_score(height, idx):
 
     return prod((score_left, score_right, score_down, score_up))
 
+def test_day8():
+    assert day8(TEST) == (21, 8)
 
-if __name__ == "__main__":
+def day8(tree_lines):
+    trees = []
+
+    y_len = len(tree_lines)
+    x_len = len(tree_lines[0])
+
     for l in tree_lines:
         trees.extend(list(map(int, l)))
 
-    part_a = sum(1 for i, t in enumerate(trees) if is_edge(i) or is_visible(t, i))
-    part_b = max(tree_score(t, i) for i, t in enumerate(trees) if not is_edge(i))
+    part_a = sum(
+        1
+        for i, t in enumerate(trees)
+        if is_edge(i, x_len, y_len) or is_visible(trees, t, i, x_len, y_len)
+    )
+    part_b = max(
+        tree_score(trees, t, i, x_len, y_len)
+        for i, t in enumerate(trees)
+        if not is_edge(i, x_len, y_len)
+    )
+    return part_a, part_b
+
+
+if __name__ == "__main__":
+    part_a, part_b = day8(lines)
 
     submit(part_a, part="a", day=8, year=2022)
     submit(part_b, part="b", day=8, year=2022)
