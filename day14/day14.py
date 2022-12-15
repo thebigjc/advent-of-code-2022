c58922adae27dbc1f608c8dcfbecacfaf0bd7ef4
Jordan Christensen <thebigjc@gmail.com>
2022-12-15 00:35:32 -0500

    day14

--- day14/day14.py
+++ day14/day14.py
@@ -3,9 +3,9 @@ from itertools import pairwise
 from collections import defaultdict
 
 class Grid:
-    def __init__(self):
+    def __init__(self, void):
+        self.void = void
         self.grid = {}
-        self.grid[(500, 0)] = "+"
         self.min_x = 500
         self.max_x = 500
         self.max_y = 0
@@ -20,26 +20,38 @@ class Grid:
             self.max_y = y
 
     def is_air(self, x, y):
+        if not self.void and y == self.max_y + 2:
+            return False
+
         return (x, y) not in self.grid
+
+    def in_bounds(self, x, y):
+        if self.void:
+            return x >= self.min_x and x <= self.max_x and y <= self.max_y
+        else:
+            return True
 
     def flow_sand(self):
         # Start at 500,0, and flow down until we hit something
         x, y = 500, 0
-        while x >= self.min_x and x <= self.max_x and y <= self.max_y:
+
+        while self.in_bounds(x, y):
             if self.is_air(x, y+1):
                 y += 1
                 continue
-            if self.grid[(x, y+1)] in ("#", "o"):
-                if self.is_air(x-1, y+1): # left and down
-                    x -= 1
-                    y += 1
-                    continue
-                if self.is_air(x+1, y+1): # right and down
-                    x += 1
-                    y += 1
-                    continue
-                self.grid[(x, y)] = "o"
-                break
+            # We hit something, so we need to check left and right
+            if self.is_air(x-1, y+1): # left and down
+                x -= 1
+                y += 1
+                continue
+            if self.is_air(x+1, y+1): # right and down
+                x += 1
+                y += 1
+                continue
+            if x == 500 and y == 0:
+                return True
+            self.grid[(x, y)] = "o"
+            return False
 
     def draw_line(self, p1, p2):
         x1, y1 = p1
@@ -65,26 +77,24 @@ class Grid:
 def parse_parts(parts):
     return [list(map(int, part.split(","))) for part in parts]
 
-#        if len(part) == 1:
-#            draw_line(part[0], grid)
-#        else:
-#            draw_line(part[0], grid)
-#            draw_line(part[1], grid)
-
-def day14_a(part_a):
+def day14(part_a, void=True):
     all_parts = []
     for l in part_a:
         parts = l.split(" -> ")
         parts = parse_parts(parts)
         all_parts.append(parts)
     
-    grid = Grid()
+    grid = Grid(void)
     grid.draw_lines(all_parts)
 
     count = 0
     while True:
         old_grid = grid.grid.copy()
-        grid.flow_sand()
+        
+        if grid.flow_sand():
+            count += 1
+            break
+
         if old_grid == grid.grid:
             break
         count += 1
@@ -95,8 +105,14 @@ TEST = """498,4 -> 498,6 -> 496,6
 503,4 -> 502,4 -> 502,9 -> 494,9""".splitlines()
 
 def test_part_a():
-    assert(day14_a(TEST) == 24)
+    assert(day14(TEST, True) == 24)
+
+def test_part_b():
+    assert(day14(TEST, False) == 93)
 
 if __name__ == "__main__":
     test_part_a()
-    #print(day14_a(lines))
+    submit(day14(lines, True), part="a", day=14, year=2022)
+
+    test_part_b()
+    submit(day14(lines, False), part="b", day=14, year=2022)
