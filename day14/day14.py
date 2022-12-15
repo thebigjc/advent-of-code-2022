1b7ed8e4a4834e8d1636183270b300dc6e20142a
Jordan Christensen <thebigjc@gmail.com>
2022-12-15 00:42:33 -0500

    day14 black

--- day14/day14.py
+++ day14/day14.py
@@ -2,6 +2,7 @@ from aocd import lines, submit
 from itertools import pairwise
 from collections import defaultdict
 
+
 class Grid:
     def __init__(self, void):
         self.void = void
@@ -36,15 +37,15 @@ class Grid:
         x, y = 500, 0
 
         while self.in_bounds(x, y):
-            if self.is_air(x, y+1):
+            if self.is_air(x, y + 1):
                 y += 1
                 continue
             # We hit something, so we need to check left and right
-            if self.is_air(x-1, y+1): # left and down
+            if self.is_air(x - 1, y + 1):  # left and down
                 x -= 1
                 y += 1
                 continue
-            if self.is_air(x+1, y+1): # right and down
+            if self.is_air(x + 1, y + 1):  # right and down
                 x += 1
                 y += 1
                 continue
@@ -60,14 +61,14 @@ class Grid:
             if y1 > y2:
                 y1, y2 = y2, y1
 
-            for y in range(y1, y2+1):
+            for y in range(y1, y2 + 1):
                 self.add_rock(x1, y)
         elif y1 == y2:
             if x1 > x2:
                 x1, x2 = x2, x1
-            for x in range(x1, x2+1):
+            for x in range(x1, x2 + 1):
                 self.add_rock(x, y1)
-    
+
     def draw_lines(self, all_pairs):
         for pairs in all_pairs:
             for pair in pairwise(pairs):
@@ -77,20 +78,21 @@ class Grid:
 def parse_parts(parts):
     return [list(map(int, part.split(","))) for part in parts]
 
+
 def day14(part_a, void=True):
     all_parts = []
     for l in part_a:
         parts = l.split(" -> ")
         parts = parse_parts(parts)
         all_parts.append(parts)
-    
+
     grid = Grid(void)
     grid.draw_lines(all_parts)
 
     count = 0
     while True:
         old_grid = grid.grid.copy()
-        
+
         if grid.flow_sand():
             count += 1
             break
@@ -98,17 +100,21 @@ def day14(part_a, void=True):
         if old_grid == grid.grid:
             break
         count += 1
-    
+
     return count
+
 
 TEST = """498,4 -> 498,6 -> 496,6
 503,4 -> 502,4 -> 502,9 -> 494,9""".splitlines()
 
+
 def test_part_a():
-    assert(day14(TEST, True) == 24)
+    assert day14(TEST, True) == 24
+
 
 def test_part_b():
-    assert(day14(TEST, False) == 93)
+    assert day14(TEST, False) == 93
+
 
 if __name__ == "__main__":
     test_part_a()
